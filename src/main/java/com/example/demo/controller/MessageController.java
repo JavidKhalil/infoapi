723099eba1f66ed9f1477e5f1311a0333c5b66f6
JavidKhalil <cavidxalilov@gmail.com>
2020-02-21 15:47:08 +0200

    post maaping added

--- src/main/java/com/example/demo/controller/MessageController.java
+++ src/main/java/com/example/demo/controller/MessageController.java
@@ -4,6 +4,8 @@ import com.example.demo.beans.Message;
 import com.example.demo.config.MongoDBConnector;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,6 +19,11 @@ public class MessageController {
     @GetMapping("/")
     public List<Message> getMessages(){
         return mongoRepository.findAll();
+    }
+
+    @PostMapping("/v1/add/msg")
+    public Message addMesage(@RequestBody(required = false) Message message){
+        return mongoRepository.save(message);
     }
 
 }
